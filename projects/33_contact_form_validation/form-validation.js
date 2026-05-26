27f43dbb99590cfe097dce83f1340e741baeccc0
Nguyen Trong Tuong <tuong2342006@gmail.com>
2026-05-27 00:30:57 +0700

    [DOM] Select form and input elements

---
+++ projects/33_contact_form_validation/form-validation.js
@@ -0,0 +1,20 @@
+const form =
+    document.getElementById("contactForm");
+
+const nameInput =
+    document.getElementById("name");
+
+const emailInput =
+    document.getElementById("email");
+
+const messageInput =
+    document.getElementById("message");
+
+const nameError =
+    document.getElementById("nameError");
+
+const emailError =
+    document.getElementById("emailError");
+
+const messageError =
+    document.getElementById("messageError");
