9fd88dbfc2650b6aa54512c14279ca36b8f307f7
Nguyen Trong Tuong <tuong2342006@gmail.com>
2026-05-27 00:34:03 +0700

    [CRUD] Add create and read operations

--- projects/35_todo_app_crud/app.js
+++ projects/35_todo_app_crud/app.js
@@ -8,3 +8,36 @@ const todoList =
     document.getElementById("todoList");
 
 let todos = [];
+
+function renderTodos() {
+
+    todoList.innerHTML = "";
+
+    todos.forEach(todo => {
+
+        todoList.innerHTML += `
+            <li>
+                ${todo.text}
+            </li>
+        `;
+    });
+}
+
+addBtn.addEventListener("click", () => {
+
+    const text =
+        todoInput.value.trim();
+
+    if (!text) return;
+
+    todos.unshift({
+        id: Date.now(),
+        text,
+        completed: false
+    });
+
+    todoInput.value = "";
+
+    renderTodos();
+});
+
