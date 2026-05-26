8a2af6ce6960b5b377ba9d1206f0bea82b87bf84
Nguyen Trong Tuong <tuong2342006@gmail.com>
2026-05-27 00:17:21 +0700

    [STATE] Create student data structure

---
+++ projects/04_quan_ly_sinh_vien/app.js
@@ -0,0 +1,54 @@
+const students = [
+    {
+        id: 1,
+        name: "An",
+        score: 8.5
+    },
+    {
+        id: 2,
+        name: "Binh",
+        score: 7.0
+    },
+    {
+        id: 3,
+        name: "Cuong",
+        score: 9.2
+    },
+    {
+        id: 4,
+        name: "Dung",
+        score: 6.5
+    },
+    {
+        id: 5,
+        name: "Hoa",
+        score: 8.0
+    },
+    {
+        id: 6,
+        name: "Lan",
+        score: 5.5
+    },
+    {
+        id: 7,
+        name: "Minh",
+        score: 9.0
+    },
+    {
+        id: 8,
+        name: "Nam",
+        score: 7.8
+    },
+    {
+        id: 9,
+        name: "Phuong",
+        score: 8.7
+    },
+    {
+        id: 10,
+        name: "Trang",
+        score: 6.8
+    }
+];
+
+console.log(students);
