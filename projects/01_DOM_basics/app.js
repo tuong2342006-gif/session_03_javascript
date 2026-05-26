e781548f92d9947637e66821fedb3d84dfc620dc
Nguyen Trong Tuong <tuong2342006@gmail.com>
2026-05-26 23:50:51 +0700

    [DOM] Update content dynamically

---
+++ projects/01_DOM_basics/app.js
@@ -0,0 +1,43 @@
+console.log("DOM Basics");
+
+//getElementById
+const title =
+    document.getElementById("title");
+console.log(title);
+
+//querySelector
+const desc =
+    document.querySelector(".description");
+console.log(desc);
+
+//querySelector với ID
+const btn =
+    document.querySelector("#changeBtn");
+
+//querySelectorAll
+const items =
+    document.querySelectorAll("li");
+
+//đọc nội dung
+console.log(title.textContent);
+//thay đổi nội dung
+title.textContent =
+    "JavaScript DOM Master";
+
+//innerHTML
+const desc =
+    document.querySelector(".description");
+
+desc.innerHTML =
+`
+<b>Learning DOM</b>
+`;
+
+//textContent
+desc.textContent =
+"<b>Hello</b>";
+
+//innerHTML
+desc.innerHTML =
+"<b>Hello</b>";
+
