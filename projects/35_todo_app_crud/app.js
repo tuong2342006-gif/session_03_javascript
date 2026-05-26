9cac70f11bec8e4be3017bab7d0cb7275de4441d
Nguyen Trong Tuong <tuong2342006@gmail.com>
2026-05-27 00:33:24 +0700

    [STATE] Create todo state management

---
+++ projects/35_todo_app_crud/app.js
@@ -0,0 +1,10 @@
+const todoInput =
+    document.getElementById("todoInput");
+
+const addBtn =
+    document.getElementById("addBtn");
+
+const todoList =
+    document.getElementById("todoList");
+
+let todos = [];
