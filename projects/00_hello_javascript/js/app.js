b30ed3a7573e05c313c34a0c023385e9a76059e5
Nguyen Trong Tuong <tuong2342006@gmail.com>
2026-05-26 21:35:57 +0700

    [FEATURE] Add variable and data type examples

--- projects/00_hello_javascript/js/app.js
+++ projects/00_hello_javascript/js/app.js
@@ -1,1 +1,19 @@
 console.log("Hello JavaScript");
+
+//let
+let age = 20;
+console.log(age);
+age = 21;
+console.log(age);
+//const
+const name = "Tuong";
+console.log(name);
+//var
+var city = "Ha Noi";
+//typeof
+const username = "Tuong";
+const score = 9;
+const isStudent = true;
+console.log(typeof username);
+console.log(typeof score);
+console.log(typeof isStudent);
