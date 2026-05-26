03d21fed37e5a0c4521e101066d971339a1d472a
Nguyen Trong Tuong <tuong2342006@gmail.com>
2026-05-27 00:35:08 +0700

    [CRUD] Implement delete todo feature

--- projects/35_todo_app_crud/app.js
+++ projects/35_todo_app_crud/app.js
@@ -41,3 +41,47 @@ addBtn.addEventListener("click", () => {
     renderTodos();
 });
 
+function renderTodos() {
+
+    todoList.innerHTML = "";
+
+    todos.forEach(todo => {
+
+        todoList.innerHTML += `
+            <li>
+
+                ${todo.text}
+
+                <button
+                    class="delete-btn"
+                    data-id="${todo.id}"
+                >
+                    Delete
+                </button>
+
+            </li>
+        `;
+    });
+}
+
+todoList.addEventListener("click", (e) => {
+
+    if (
+        e.target.classList.contains(
+            "delete-btn"
+        )
+    ) {
+
+        const id =
+            Number(
+                e.target.dataset.id
+            );
+
+        todos =
+            todos.filter(
+                todo => todo.id !== id
+            );
+
+        renderTodos();
+    }
+});
