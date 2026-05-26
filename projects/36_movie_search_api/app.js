ddcbe9c6363087b86d672cd5059c65d7e61106e3
Nguyen Trong Tuong <tuong2342006@gmail.com>
2026-05-27 00:45:52 +0700

    [STATE] Create movie search state management

---
+++ projects/36_movie_search_api/app.js
@@ -0,0 +1,15 @@
+const API_KEY = "YOUR_API_KEY";
+
+const searchInput =
+    document.getElementById("searchInput");
+
+const searchBtn =
+    document.getElementById("searchBtn");
+
+const movieContainer =
+    document.getElementById("movieContainer");
+
+const loading =
+    document.getElementById("loading");
+
+let currentPage = 1;
